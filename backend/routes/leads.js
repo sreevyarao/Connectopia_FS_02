0ae9f66a1d50a1fd87e2acf11d68000727562088
S.Sreevya Rao <sreevyarao@gmail.com>
2026-05-02 23:23:06 +0530

    Update leads.js

--- backend/routes/leads.js
+++ backend/routes/leads.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const Lead    = require('../models/Lead');
+const Lead    = require('../models/lead');
 const protect = require('../middleware/auth');
 
 // All routes protected
